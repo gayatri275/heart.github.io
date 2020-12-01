67bffd9b60133bef536f113ed8e66aa1359c2ebc
gayatri275 <60381288+gayatri275@users.noreply.github.com>
2020-12-01 13:27:38 +0530

    myheart

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>HELLOW WORLD</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta http-equiv="X-UA-Compatible" content="IF-edge">
+<meta name="viewport" content="width-device-width,initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+</head>
+<body>
+	<div class="back"></div>
+	<div class="area">
+	<div class="one">
+		Happy Birthday
+	</div>
+	<div class="name">Yogesh</div>
+	<div class="det">
+		Heyyyy Stranger a Little msg for you... 
+		Hope you like it Today on your Birthday Really I am missing you more....
+		I love you ...
+		Its true you are my Happiness...
+		Thank you for coming in my life...
+		I am very far away from you..
+		But you knows How much you are close to me...
+		Happy Birthday to you My Teddy bear..from bottom of my heart...
+
+
+
+	</div>
+</div>
+	<div class="imgarea">
+		<div class="img left">
+		<div data-aos="zoom-in"></div>
+		<img src="pic2.jpg">
+	</div>
+</div>
+<div class="img right">
+		<div data-aos="zoom-in"></div>
+		<img src="pic1.jpg">
+	</div>
+</div>
+<div class="img left">
+		<div data-aos="zoom-in"></div>
+		<img src="pic5.png">
+	</div>
+</div>
+<div class="img right">
+		<div data-aos="zoom-in"></div>
+		<img src="pic3.jpg">
+	</div>
+</div>
+<div class="img left">
+		<div data-aos="zoom-in"></div>
+		<img src="pic4.jpg">
+	</div>
+</div>
+<script>
+  AOS.init();
+</script>
+</body>
+</html>
